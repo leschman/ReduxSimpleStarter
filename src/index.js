241db92e57a8c9a149bc3cafcc8b8e54faa2330f
Logan Esch <lesch@sofi.org>
2018-08-09 08:28:43 -0600

    lesson-1-11: created new src dir and got text to render.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This componenent should produce some HTML
+ const App = () => {
+	 return <div>Hi!</div>;
+ }
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Take this compnent's genrated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
