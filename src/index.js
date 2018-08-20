8798ec6057efb98084eb1aa4a60fbde72236733b
Logan Esch <lesch@sofi.org>
2018-08-20 07:45:59 -0600

    lesson-1-14: add api key and search_bar component.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDIlpWTXuoMYd-BtCU7lkPVdpLxn1_ZOt0';
+
 // Create a new component. This componenent should produce some HTML
- const App = () => {
-	 return <div>Hi!</div>;
- }
+const App = () => {
+	return (
+        <div>
+            <SearchBar />
+        </div>
+     );
+}
 
 // Take this compnent's genrated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
