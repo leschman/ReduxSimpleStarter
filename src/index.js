16ca4e5a7a1a288b3fb2b45c6847d9e657b5c4fe
Logan Esch <lesch@sofi.org>
2018-09-10 14:30:24 -0600

    lesson-2-26: build out video list to display title and thumbnail of videos.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDIlpWTXuoMYd-BtCU7lkPVdpLxn1_ZOt0';
 
@@ -23,6 +24,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
