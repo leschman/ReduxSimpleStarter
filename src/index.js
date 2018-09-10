ac1fd527645672cf6614ab483b4bfae59077bd50
Logan Esch <lesch@sofi.org>
2018-09-10 13:45:06 -0600

    lesson-2-23: convert App to a controlled component.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDIlpWTXuoMYd-BtCU7lkPVdpLxn1_ZOt0';
 
+
 // Create a new component. This componenent should produce some HTML
-const App = () => {
-	return (
-        <div>
-            <SearchBar />
-        </div>
-     );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });    
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this compnent's genrated HTML and put it on the page (in the DOM)
