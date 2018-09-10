87489b415c0cfcad6d8b589efcf7e0d461aee917
Logan Esch <lesch@sofi.org>
2018-09-10 15:55:45 -0600

    lesson-2-31: add css. hook up searchbar to search for videos.

--- src/index.js
+++ src/index.js
@@ -17,9 +17,13 @@ class App extends Component {
     this.state = { 
       videos: [],
       selectedVideo: null 
-    };
+    };  
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');  
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ 
         videos: videos, 
         selectedVideo: videos[0]
@@ -30,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
